3f623db6a5867481e8341252a1179b062a72e5a0
JamesMcDougallJr <jmcdouga@ucsd.edu>
2021-03-02 11:01:25 -0800

    Update with arduino code

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
+echo "Flashing Teensy"
+#https://github.com/PaulStoffregen/teensy_loader_cli
+./teensy_loader/teensy_loader_cli --mcu=mk20dx256 -w ./Arduino/teensy-test/teensy-test.ino.TEENSY32.hex -v # only for teensy 3.2
+sleep 2
+echo "Start Flask Server to publish voltages"
 cd server && python3 -m flask run &> /dev/null &
 sleep 2
+echo "Start vidom server to host plots"
 visdom -port 5001 &
 sleep 2
+echo "Start visdom publisher to publish data to plots."
 python3 client/visdom_publisher.py &> /dev/null &
